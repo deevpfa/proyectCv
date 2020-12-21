51d9702e7a638d16f32b09e92e4b434320bd6b17
patofara <patriciofara2@gmail.com>
2020-12-21 16:28:36 -0300

    a

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let header = document.getElementById("header")
 header.style.opacity = "1"
     //cambio de imagenes de proyectos
 let imagenGif = document.getElementById("imagen-gifs");
-imagenGif.addEventListener("mouseover", function() {imagenGif.src="/assets/gifos2.png"})
-imagenGif.addEventListener("mouseleave", function(){imagenGif.src="/assets/gifos1.jpeg"})
+imagenGif.addEventListener("mouseover", () =>  {imagenGif.src="./assets/gifos2.png"})
+imagenGif.addEventListener("mouseleave", () => {imagenGif.src="./assets/gifos1.jpeg"})
 
 let landing = document.getElementById("imagen-landing");
-landing.addEventListener("mouseover", function(){landing.src="/assets/responsive.png"})
-landing.addEventListener("mouseleave", function(){landing.src="/assets/dinamicPage.jpeg"})
+landing.addEventListener("mouseover", () => {landing.src="./assets/responsive.png"})
+landing.addEventListener("mouseleave", () => {landing.src="./assets/dinamicPage.jpeg"})
